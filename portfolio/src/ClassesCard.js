cb17af144219cf8ed9b27be0f1fc7bbbd989ae48
acolbourn <alexcolbourn@example.com>
2020-05-28 20:41:33 -0700

    Refine responsive grid for classes route

--- portfolio/src/ClassesCard.js
+++ portfolio/src/ClassesCard.js
@@ -2,22 +2,29 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
+// import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
+// import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import './styles/ribbon.css';
 
 const useStyles = makeStyles({
   root: (props) => ({
-    width: 345,
-    height: 308,
     border: props.featured ? '1px solid #08fdd8' : 'none',
     position: 'relative',
+    height: 308,
+    width: 330,
+    '@media (max-width: 340px)': {
+      width: 300,
+    },
   }),
   media: {
     height: 140,
+  },
+  bulletPoints: {
+    listStylePosition: 'inside',
+    paddingLeft: 0,
   },
 });
 
@@ -30,14 +37,14 @@ export default function ClassesCard(props) {
       <CardActionArea>
         <CardMedia className={classes.media} image={imgSrc} title={imgAlt} />
         <CardContent>
-          <Typography gutterBottom variant='h5' component='h2'>
+          <Typography gutterBottom variant='h6' component='h2'>
             {title}
           </Typography>
-          <Typography gutterBottom variant='subtitle2'>
+          <Typography gutterBottom variant='subtitle1'>
             {school}
           </Typography>
           <Typography variant='body2' color='textSecondary' component='span'>
-            <ul>
+            <ul className={classes.bulletPoints}>
               {keyPoints.map((point, idx) => (
                 <li key={idx}>{point}</li>
               ))}

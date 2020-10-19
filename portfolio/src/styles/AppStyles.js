3175a5d056bbce17f928be6811674da6d27bfd92
acolbourn <alexcolbourn@example.com>
2020-10-18 19:39:00 -0700

    Start font search

--- portfolio/src/styles/AppStyles.js
+++ portfolio/src/styles/AppStyles.js
@@ -1,10 +1,24 @@
 import { makeStyles, createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
+import GilroyLight from '../fonts/Gilroy-Light/font.woff2'
+
+const gilroy = {
+  fontFamily: 'Gilroy',
+  fontStyle: 'normal',
+  fontDisplay: 'swap',
+  fontWeight: 400,
+  src: `
+    local('Gilroy'),
+    local('Gilroy-Light'),
+    url(${GilroyLight}) format('woff2')
+  `,
+};
 
 const colors = {
   background: '#1D1D1D',
   // primary: '#91C3DC', // light blue from retirement site
   // primary: '#4086AA', // dark blue from retirement site
-  primary: '#29BFF7', 
+  // primary: '#3EA6FF',  // from youtube
+  primary: '#29BFF7', // MAIN
   // primary: '#08fdd8', // green from jj site
   // primary: "#FD1056", // red from jj site
   secondary: '#444',
@@ -23,9 +37,19 @@ let theme = createMuiTheme({
   },
   fonts: {
     primary: 'Montserrat, sans-serif',
-  },
+  },  
   // Material-UI global overrides
+  typography: {
+    fontFamily: 'Gilroy, sans-serif',
+    // google fonts: Karla Rubik Work Sans
+    // fontFamily: 'Montserrat, sans-serif',
+  },
   overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        '@font-face': [gilroy],
+      },
+    }, 
     MuiButton: {            
       containedPrimary: {        
         backgroundColor: colors.fadedBlue,

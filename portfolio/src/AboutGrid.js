45752f04ca40b13961e40497f8ee8541eab98885
acolbourn <alexcolbourn@example.com>
2020-10-26 18:16:24 -0700

    Make about page responsive

--- portfolio/src/AboutGrid.js
+++ portfolio/src/AboutGrid.js
@@ -13,14 +13,23 @@ const useStyles = makeStyles((theme) => ({
     fontFamily: 'AvenirNextCyr, ProximaNova, sans-serif',
     fontWeight: '400',
     display: 'grid',
+    gridTemplateRows: 'repeat(2, minmax(min-content, 50%))',
     gridTemplateColumns: '1fr 1fr',
-    gridTemplateRows: '1fr 1fr',
     gridTemplateAreas: `
             'about webSkills'
             'timeline roboticsSkills'
         `,
+    [theme.breakpoints.down('md')]: {
+        gridTemplateRows: 'repeat(3, minmax(min-content, 33%))',
+        gridTemplateColumns: '1fr 1fr',
+        gridTemplateAreas: `
+                'about webSkills'
+                'about roboticsSkills'
+                'timeline timeline'
+            `
+    },
     [theme.breakpoints.down('sm')]: {
-        gridTemplateRows: '1fr 1fr',
+        gridTemplateRows: 'repeat(2, minmax(min-content, 50%))',
         gridTemplateColumns: '1fr',
         gridTemplateAreas: `
                 'about'
@@ -29,23 +38,49 @@ const useStyles = makeStyles((theme) => ({
     },
     gap: '16px',
     padding: '16px',
-    
+    [theme.breakpoints.down('md')]: {
+      gap: '10px',
+      padding: '10px',
+    },
+    '@media (max-height: 750px)': {
+      gap: '10px',
+      padding: '10px',
+    },        
   },  
   gridItem: {
     backgroundColor: theme.colors.background,
     border: '1px solid',
     borderRadius: '4px',
     borderColor: theme.colors.fadedBlue,
-    padding: theme.spacing(2),
     textAlign: 'center',
     color: theme.palette.text.secondary,
     width: '100%',
     height: '100%',
     display: 'flex',
     flexDirection: 'column',
+    padding: '16px',
+    [theme.breakpoints.down('md')]: {
+      padding: '10px',
+    },
+    '@media (max-height: 750px)': {
+      padding: '10px',
+    },
+    [theme.breakpoints.down('xs')]: {      
+      paddingBottom: '20px',
+    },
   },
   about: {
       gridArea: 'about',
+      fontSize: '1rem',
+    [theme.breakpoints.down('md')]: {
+      fontSize: '0.8rem',
+    },
+    [theme.breakpoints.down('xs')]: {      
+      fontSize: '0.8rem',
+    },
+    '@media (max-height: 890px)': {
+      fontSize: '0.7rem',
+    },
   },
   timeline: {
       gridArea: 'timeline',
@@ -59,7 +94,23 @@ const useStyles = makeStyles((theme) => ({
   title: {
     color: theme.colors.primary,
     fontFamily: 'AvenirNextCyr, ProximaNova, sans-serif',
-    fontWeight: '300'
+    fontWeight: '300',
+    fontSize: '42px',
+    [theme.breakpoints.down('md')]: {
+      fontSize: '35px',
+      '@media (max-height: 890px)': {
+        fontSize: '25px',
+      },
+    },
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '35px',      
+    },
+    [theme.breakpoints.down('xs')]: {      
+      fontSize: '35px',
+    },
+    '@media (max-height: 750px)': {
+      fontSize: '25px',
+    },   
   },
   content: {
     flex: 1,    
@@ -67,28 +118,46 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'column',
     justifyContent: 'center',
     margin: '16px 16px 0 16px',
+    [theme.breakpoints.down('md')]: {
+      margin: '8px 16px 0 16px',
+    },
     '& .MuiTypography-root': {
       fontFamily: 'AvenirNextCyr, ProximaNova, sans-serif',
       fontWeight: '400'
+    },
+    '& .MuiTypography-body1': {
+      fontSize: '1rem',
+      [theme.breakpoints.down('md')]: {
+        fontSize: '0.9rem',      
+      },
+      [theme.breakpoints.down('sm')]: {
+        fontSize: '1rem',      
+      },
     }    
   },
   skillGroupBox: {
     height: '100%',
     width: '100%',
     display: 'grid',  
-    gridTemplateRows: 'repeat(3, 1fr)',
-    gridTemplateColumns: 'repeat(4, 1fr)',  
-    justifyItems: 'center',
-    alignItems: 'center',
+    gridTemplateRows: 'repeat(3, minmax(min-content, 33%))',
+    gridTemplateColumns: 'repeat(4, 1fr)', 
+    gap: '4px', 
+    justifyItems: 'stretch',
+    alignItems: 'stretch',
     justifyContent: 'space-around',
-    alignContent: 'space-around',
+    alignContent: 'space-around',      
   },
-  skillBox: {
-    // whiteSpace: 'nowrap',    
-    margin: '8px',
-  },
-
- 
+  skillBox: {      
+    margin: 0,
+    backgroundColor: '#303030',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center', 
+    fontSize: '40px',
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '35px',      
+    },
+  }, 
 }));
 
 export default function AboutGrid() {
@@ -97,7 +166,7 @@ export default function AboutGrid() {
     const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
     const webSkills = ['JavaScript', 'Python', 'React', 'HTML 5', 'CSS 3', 'WebGL', 'Three.js', 'D3.js', 'Node', 'Express', 'MongoDB', 'Git'];
-    const roboticsSkills = ['ROS', 'Gazebo', 'LabVIEW', "PLC's", 'Microcontrollers',  'Raspberry Pi', 'Arduino', 'AutoCAD', 'SolidWorks', 'Mechanical Design', '3d Printing', 'Electrical Schematics'];    
+    const roboticsSkills = ['ROS', 'Gazebo', 'LabVIEW', "PLC's", 'Microcontrollers', 'Arduino', 'Raspberry Pi','AutoCAD', 'SolidWorks', 'Mechanical Design', '3d Printing', 'Electrical Schematics'];    
 
     return (
         <div className={classes.root}>
@@ -120,7 +189,7 @@ export default function AboutGrid() {
                       moment, but I'm very interested in all Backend, Fullstack, and
                       Robotics software opportunities as well.
                     </Typography>
-                    <Typography paragraph variant='body1' gutterBottom={false}>
+                    <Typography paragraph variant='body1' style={{marginBottom: '2px'}}>
                       Outside of work you'll find me making music, hiking, biking, or
                       flying planes again if you pay me enough.
                     </Typography>

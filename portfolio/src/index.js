46129c3c7586931ac9e47277d630d4927a56402e
Alex Colbourn <alexcolbourn@gmail.com>
2024-11-12 20:28:04 -0800

    Update pdf viewer, react libraries, and build script

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,9 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
 
 serviceWorker.unregister();
